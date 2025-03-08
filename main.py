7b6f68d09dc212228b0e2b89906a40fa23c2bedb
Hagbarn <am.matteo25@gmail.com>
2025-03-08 23:12:19 +0800

    Added Player management

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,11 +8,13 @@ def main():
     black_color = (0, 0, 0)
     pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black_color)
+        player.draw(screen)
         pygame.display.flip()
         dt = pygame.time.Clock().tick(60)/1000 # 60 FPS
 
