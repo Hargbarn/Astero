dd4f3d733d95a619fa2592c8b84a7df6ce5e73cd
Hagbarn <am.matteo25@gmail.com>
2025-03-08 22:24:22 +0800

    Adding FPS limit to 60 FPS to reduce resource consumption

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black_color = (0, 0, 0)
+    pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black_color)
         pygame.display.flip()
+        dt = pygame.time.Clock().tick(60)/1000 # 60 FPS
 
 
 
