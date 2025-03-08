0b1bed2d8df3fb1a51c25ab49fb0b40757ad32f1
Hagbarn <am.matteo25@gmail.com>
2025-03-08 23:30:47 +0800

    Adding Player movements

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black_color = (0, 0, 0)
-    pygame.time.Clock()
+    time = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
         screen.fill(black_color)
         player.draw(screen)
         pygame.display.flip()
-        dt = pygame.time.Clock().tick(60)/1000 # 60 FPS
+
+        dt = time.tick(60)/1000 # 60 FPS
 
 
 
