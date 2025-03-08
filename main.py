dc0ab79c79527041534c1589a95da6eb0fdf9f38
Hagbarn <am.matteo25@gmail.com>
2025-03-08 23:54:33 +0800

    Added Asteroids management

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from asteroid import Asteroid
+from asteroidspawn import AsteroidField
 from constants import *
 from player import Player
 
@@ -8,6 +10,16 @@ def main():
     black_color = (0, 0, 0)
     time = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    asteroid = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -15,9 +27,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill(black_color)
-        player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
 
         dt = time.tick(60)/1000 # 60 FPS
